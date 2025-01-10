74d30afd3bdb63a735ceef6c6640a360bb83e68e
TigranZakharyan <zakharyantigran27@gmail.com>
2025-01-10 20:27:22 +0400

    added project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,5 @@
 import mock10 from '../assets/images/mock10.png';
+import mock9 from '../assets/images/mock9.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -10,6 +11,11 @@ function Project() {
                 <a href="https://tigranzakharyan.github.io/global/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://tigranzakharyan.github.io/global/" target="_blank" rel="noreferrer"><h2>GlobalUI</h2></a>
                 <p>A website showing my Front End skills, the application is created with HTML, SASS and JS languages using Gulp builder</p>
+            </div>
+            <div className="project">
+                <a target="_blank" rel="noreferrer"><img src={mock9} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a target="_blank" rel="noreferrer"><h2>ActradeMaster</h2></a>
+                <p>Initial website of ActradeSoft company with all the functionalities needed</p>
             </div>
         </div>
     </div>
