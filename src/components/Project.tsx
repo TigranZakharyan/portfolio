3e55e2dbe62534297dd53bad053b04a82a7df83b
TigranZakharyan <zakharyantigran27@gmail.com>
2025-01-11 17:51:31 +0400

    added one project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,6 @@
 import mock10 from '../assets/images/mock10.png';
 import mock9 from '../assets/images/mock9.png';
+import mock8 from '../assets/images/mock8.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -16,6 +17,11 @@ function Project() {
                 <a target="_blank" rel="noreferrer"><img src={mock9} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a target="_blank" rel="noreferrer"><h2>ActradeMaster</h2></a>
                 <p>Initial website of ActradeSoft company with all the functionalities needed</p>
+            </div>
+		<div className="project">
+                <a target="_blank" rel="noreferrer"><img src={mock8} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a target="_blank" rel="noreferrer"><h2>Authentication</h2></a>
+                <p>Cross platform mobile application</p>
             </div>
         </div>
     </div>
